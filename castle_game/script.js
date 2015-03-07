dfda195d42b789b1f1eaf23526d93df898701db0
Mystylaise <mystylaise@gmail.com>
2015-03-07 17:26:58 +0100

    Starting files
    Antho, j'ai créer les 4 fichiers de départs pour pouvoir commencer le
    travail.

---
+++ castle_game/script.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'canva', { preload: preload, create: create, update: update });
+
+function preload() {
+
+} //end function preload
+
+function create() {
+
+} //end function create
+
+function update() {
+
+} //end function update
+
+function render() {
+
+} //end function render
