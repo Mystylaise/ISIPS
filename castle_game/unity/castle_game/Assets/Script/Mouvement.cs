d4faf98d30edc99f9606ad0312c67130cdaa7530
Anthweks <eksterowicz.anthony@gmail.com>
2015-06-23 04:02:39 +0200

    premier changement Unity , il est 4h00 du mat

---
+++ castle_game/unity/castle_game/Assets/Script/Mouvement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouvement : MonoBehaviour {
+
+	public float vitesse = 1f;
+
+	private Animator anim;
+
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+
+	
+	}
+	
+
+	void Update () {
+
+		transform.rotation = Quaternion.Euler (0, 0, 0);	
+
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+			//transform.position += new Vector3(-0.05f,0,0);
+			transform.position -= new Vector3 (vitesse * Time.deltaTime, 0, 0);
+			transform.localScale = new Vector3 (-1, 1, 1);
+
+
+			anim.SetFloat ("vitesse", 2);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			//this.GetComponent<Rigidbody2D>().AddForce(new Vector2(4,0));/
+			//Time.deltaTime = regule La vitesse peu importe LayerMask puissance du pc
+			transform.position += new Vector3 (vitesse * Time.deltaTime, 0, 0);
+			transform.localScale = new Vector3 (1, 1, 1);
+
+			anim.SetFloat ("vitesse", 2);
+		} 
+
+
+
+		else {
+			anim.SetFloat ("vitesse", 0);
+		}
+
+		}//finUpdate
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//si le tag de l'objet qui rentre en collision avec This est "sol"
+		if (other.gameObject.tag == "glace")
+		{
+			if (Input.GetKey (KeyCode.LeftArrow)){
+			this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-50,0));
+			}
+			if (Input.GetKey (KeyCode.RightArrow)){
+				this.GetComponent<Rigidbody2D>().AddForce(new Vector2(50,0));
+			}
+			Debug.Log ("tu est entrer en collision avec la glace"); 
+		}
+
+
+
+			
+	}
+
+
+
+} 
---
+++ castle_game/unity/castle_game/Assets/Script/checkTag.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkTag : MonoBehaviour {
+
+	public bool SurEchelle;
+	public float climbSpeed;
+	private float climbVelocity;
+	private float gravityStore;
+	private Rigidbody2D myrigidbody2d;
+	// Use this for initialization
+	void Start () {
+
+		myrigidbody2d = GetComponent<Rigidbody2D> ();
+		gravityStore = myrigidbody2d.gravityScale;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.CompareTag("sol")){
+			Debug.Log ("tu es sur le sol");}
+
+		if (SurEchelle) {
+			myrigidbody2d.gravityScale = 0f;
+			climbVelocity = climbSpeed * Input.GetAxisRaw("Vertical");
+			myrigidbody2d.velocity = new Vector2(myrigidbody2d.velocity.x, climbVelocity);
+
+		}
+
+		if (!SurEchelle) {
+			myrigidbody2d.gravityScale = gravityStore;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//tu peux tester de différentes manieres
+
+		// 1- Soit l'objet avec lequel la collision s'effectue est taggé, dans ce cas tu peux utiliser son tag comme condition :
+		if (other.gameObject.tag == "sol")
+		{
+			Debug.Log ("tu es entrer en collision"); // attention: gameObject et pas GameObject
+		}
+	}
+
+
+}
