631f839f623e419a9324d2be09302a47bb790faa
Anthweks <eksterowicz.anthony@gmail.com>
2015-06-23 11:59:07 +0200

    C'est la Fin Des Haricots :'(
    Pour un dernier commit où je te met la misère 7299 changement :+1:

---
+++ isips/castle_game/Assets/Script/ZoneLevier.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneLevier : MonoBehaviour {
+
+	private checkTag player;
+	public Animator AnimLevier;
+	public BoxCollider2D edgefini;
+
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<checkTag> ();
+		AnimLevier = GetComponent<Animator> ();
+		edgefini = GetComponent<BoxCollider2D> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "chevalier")
+		{
+			player.bordLevier = true;
+
+			Debug.Log("tu est pres du levier");
+			AnimLevier.enabled = true;
+			edgefini.enabled=false;
+
+
+		}
+
+
+	}
+	
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "chevalier")
+		{
+			player.bordLevier = false;
+		}
+	}
+}
---
+++ isips/castle_game/Assets/Script/ZoneTower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneTower : MonoBehaviour {
+	
+	private checkTag player;
+	public MeshRenderer RenduTour;
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<checkTag> ();
+		RenduTour = GetComponent<MeshRenderer> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "chevalier")
+		{
+			player.InTower = true;
+			Debug.Log (" tu entre dans la tour");
+			RenduTour.enabled=false;
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "chevalier")
+		{
+			player.InTower = false;
+			RenduTour.enabled=true;
+		}
+	}
+}
